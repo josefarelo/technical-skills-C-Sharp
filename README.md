d1012dfb21217f242db2c85ef80934962c923fbe
joseF <jose.farelo@hotmail.com>
2025-04-22 21:27:45 -0300

    01_Fundamenos de C#:     id: 0001     title: Literal values

---
+++ README.md
@@ -0,0 +1,8 @@
+**Este repositorio contiene una colección completa de ejercicios de C#, organizados en módulos temáticos.**
+
+## 01 - Fundamentos de C#
+
+- literal-values.cs
+
+_Conocer unos cinco o seis tipos de datos._
+
---
+++ fundamentos-de-c#/literal-values.cs
@@ -0,0 +1,18 @@
+// IMPRESIÓN DE VALORES LITERALES
+
+
+// Literales de caracteres
+Console.WriteLine('a');    // char (carácter)
+Console.WriteLine("b");    // string (cadena de texto)
+
+// Literales de enteros
+Console.WriteLine(123);    // int (integer/entero)
+
+// Literales de punto flotante
+Console.WriteLine(0.25F);       // float ~6-9 dígitos
+Console.WriteLine(2.625);       // double ~15-17 dígitos
+Console.WriteLine(12.39816M);   // decimal 28-29 dígitos
+
+// Literales booleanos
+Console.WriteLine(true);    // True
+Console.WriteLine(false);   // False
